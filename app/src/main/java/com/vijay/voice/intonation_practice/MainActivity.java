7bd8ca79819845997e4aeb89aa18bc9f459c0862
hollysh <34343188+hollysh@users.noreply.github.com>
2018-07-10 17:34:55 +0800

    create AudioTrack instance

--- app/src/main/java/com/vijay/voice/intonation_practice/MainActivity.java
+++ app/src/main/java/com/vijay/voice/intonation_practice/MainActivity.java
@@ -1,14 +1,29 @@
 package com.vijay.voice.intonation_practice;
 
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 
 public class MainActivity extends AppCompatActivity {
 
+
+    AudioTrack mAudioTrack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAudioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
+                44100,
+                AudioFormat.CHANNEL_OUT_MONO,
+                AudioFormat.ENCODING_PCM_16BIT,
+                1024,
+                AudioTrack.MODE_STREAM
+        );
+
+
     }
 }
